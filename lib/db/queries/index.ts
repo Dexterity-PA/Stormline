2fb9564ffc72b83b712b39289cd692cf3d8fa3ef
Praneeth Annapureddy <praneeth.a2027@gmail.com>
2026-04-18 16:57:16 -0700

    feat(db): barrel export for all query domains

---
+++ lib/db/queries/index.ts
@@ -0,0 +1,7 @@
+export * from './organizations';
+export * from './members';
+export * from './indicators';
+export * from './briefings';
+export * from './alerts';
+export * from './feedback';
+export * from './dashboard';
